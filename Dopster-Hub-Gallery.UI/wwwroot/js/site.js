0f21bbe31451d34a260e93457d9c8e101e8d67c4
rape055 <41070302+rape055@users.noreply.github.com>
2021-11-03 18:38:28 -0300

    Inclusão da navegação de filtro na galeria e ajustes nos cards

--- Dopster-Hub-Gallery.UI/wwwroot/js/site.js
+++ Dopster-Hub-Gallery.UI/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+import React, { Component } from 'react';
+
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+export default class App extends Component {
+    state = {
+        data: [
+            { id: 0, full_name: 'Repo 1' },
+            { id: 1, full_name: 'Repo 2' },
+            { id: 2, full_name: 'Repo 3' },
+            { id: 3, full_name: 'Repo 4' },
+            { id: 4, full_name: 'Repo 5' },
+        ],
+    };
+
+    renderItem = ({ item }) => (
+        <View style={styles.listItem}>
+            <Text>{item.full_name}</Text>
+        </View>
+    );
+
+    render() {
+        return (
+            <FlatList style={{ marginTop: 30 }}
+                contentContainerStyle={styles.list}
+                data={this.state.data}
+                renderItem={this.renderItem}
+                keyExtractor={item =>
+                    item.id}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    list: {
+        paddingHorizontal: 20,
+    },
+
+    listItem: {
+        backgroundColor: '#EEE',
+        marginTop: 20,
+        padding: 30,
+    },
+});
+
+
+
